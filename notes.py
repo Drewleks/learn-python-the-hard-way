b75b13ace322068aacb8602b7a60f9b02ad5329e
Andrey A <drew.leks@gmail.com>
2020-07-11 21:54:31 +0300

    add ex1

---
+++ notes.py
@@ -0,0 +1,6 @@
+# Видео:
+# https://www.informit.com/promotions/book-registration-learn-more-python-3-the-hard-way-141499
+
+# https://www.informit.com/store/learn-python-3-the-hard-way-a-very-simple-introduction-9780134692883
+
+# https://eksmo.ru/files/shaw_python3.zip.
