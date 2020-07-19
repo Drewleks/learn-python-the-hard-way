9cc4d7c462f70293570321a866fa53dc3709273f
Andrey A <drew.leks@gmail.com>
2020-07-19 20:00:48 +0300

    add ex27

---
+++ ex27/ex27.py
@@ -0,0 +1,59 @@
+print("======================")
+print("NOT Истина?")
+print("not False", not False)
+print("not True", not True)
+print("======================\n")
+
+
+print("======================\n")
+print("OR Истина?")
+print("True or False", not False)
+print("True or True", True or True)
+print("False or True", False or True)
+print("False or False", False or False)
+print("======================\n")
+#
+
+print("======================")
+print("AND Истина?")
+print("True and False", False or False)
+print("True and True", False or False)
+print("False and True", False or False)
+print("False and False", False or False)
+print("======================\n")
+
+
+print("======================")
+print("NOT OR Истина?")
+print("not (True or False)", not (True or False))
+print("not (True or True)", not (True or True))
+print("not (False or True)", not (False or True))
+print("not (False or False)", not (False or False))
+print("======================\n")
+
+
+print("======================")
+print("NOT AND Истина?")
+print("not (True and False)", not (True and False))
+print("not (True and True)", not (True and True))
+print("not (False and True)", not (False and True))
+print("not (False and False)", not (False and False))
+print("======================\n")
+
+
+print("======================")
+print("!= Истина?")
+print("1 != O", 1 != 0)
+print("1 != 1", 1 != 1)
+print("0 != 1", 0 != 1)
+print("0 != 0", 0 != 0)
+print("======================\n")
+
+
+print("======================")
+print("== Истина?")
+print("1 == 0", 1 == 0)
+print("1 == 1", 1 == 1)
+print("0 == 1", 0 == 1)
+print("0 == 0", 0 == 0)
+print("======================\n")
